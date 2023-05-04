53be91a73f369dec932f7e7daf7add3305b1e33a
unknown <trantrongthiep2002@gmail.com>
2023-05-04 18:34:17 +0700

    first commit

---
+++ js/datajson.js
@@ -0,0 +1,317 @@
+let sp1 = {
+    masp: 1,
+    tensp: "BỘ 5 CHIẾC DAO BẾP CAO CẤP KATANA BASIC - SET 5 KATASET5 ĐA NĂNG - THÁI THỊT CÁ (5 CHIẾC)",
+    hinhanh: './img/Dao/katana.webp',
+    soluong: 5,
+    dongia: 600000,
+    mota: '- Bộ gồm 5 sản phẩm dao với kích cỡ khác nhau, tiện lợi cho nhiều mục đích sử dụng' + '<br>' +
+        '- Góc mài 16 độ tạo nên độ sắc hoàn hảo cho lưỡi dao, cùng với độ chống dính, giúp giảm lực của tay khi cắt thực phẩm, phù hợp để thái nhiều loại thực phẩm nhanh chóng mà ít tốn sức' +'<br>' +
+        'Thông tin sản phẩm:'+'<br>' +
+        '- Chiều dài lưỡi: 170 - 165 - 180 - 120 - 100 mm'+'<br>' +
+        '- Chiều rộng lưỡi:445 - 445 - 445 - 25 - 30 mm'+'<br>' +
+        '- Tổng chiều dài:280 -285 - 300 - 235 - 211mm'+'<br>' +
+        '- Độ dày lưỡi: 2 - 2 - 2 - 2 - 2mm'+'<br>' +
+        '- Độ dày cán dao: 20 - 20 - 20 - 20mm'+'<br>' +
+        '- Trọng lượng:103 - 120 - 49- 42 -107g'+'<br>' +
+        '- Chất liệu: Thép Nhật Bản SUS 420J2 và cán dao bằng nhựa'+'<br>' +
+        '- Thương hiệu: KATANA'+'<br>' +
+        '- Xuất xứ : Việt Nam' +'<br>'
+}
+
+let sp2 = {
+    masp: 2,
+    tensp: "BỘ 2 CHIẾC DAO BẾP CAO CẤP THƯƠNG HIỆU KATANA ESSENTIAL - DAO THÁI THỊT CÁ - DAO ĐA NĂNG - KATA220",
+    hinhanh: './img/Dao/hop2katana.webp',
+    soluong: 5,
+    dongia: 789000,
+    mota:
+        'Thông tin sản phẩm:' + '<br>' +
+        'DAO CHEF THÁI THỊT CÁ' +'<br>' +
+        '- Chiều dài lưỡi: 180 mm' +'<br>' +
+        '- Chiều rộng lưỡi: 43 mm' +'<br>' +
+        '- Tổng chiều dài: 300 mm' +'<br>' +
+        '- Độ dày sống dao: 1-1.5 mm' +'<br>' +
+        '-- Độ dày cán dao: 15 mm' +'<br>' +
+        '- Trọng lượng: 152g'+'<br>' +
+        'DAO SANTOKU THÁI ĐA NĂNG:' +'<br>' +
+        '- Chiều dài lưỡi: 165 mm' +'<br>' +
+        '- Chiều rộng lưỡi: 47 mm' +'<br>' +
+        '- Tổng chiều dài: 295 mm' +'<br>' +
+        '- Độ dày sống dao: 1-1.5 mm' +'<br>' +
+        '- Độ dày cán dao: 10 mm' +'<br>' +
+        '- Trọng lượng: 157g' 
+}
+
+let sp3 = {
+    masp: 3,
+    tensp: "BỘ 3 CHIẾC DAO BẾP CAO CẤP THƯƠNG HIỆU KATANA ESSENTIAL - DAO THÁI THỊT CÁ - DAO ĐA NĂNG - DAO GỌT HOA QUẢ - KATA200",
+    hinhanh: './img/Dao/katana3.webp',
+    soluong: 5,
+    dongia: 1117000,
+    mota:
+    'Thông tin sản phẩm:' + '<br>' +
+    'DAO CHEF THÁI THỊT CÁ' +'<br>' +
+    '- Chiều dài lưỡi: 180 mm' +'<br>' +
+    '- Chiều rộng lưỡi: 43 mm' +'<br>' +
+    '- Tổng chiều dài: 300 mm' +'<br>' +
+    '- Độ dày sống dao: 1-1.5 mm' +'<br>' +
+    '-- Độ dày cán dao: 15 mm' +'<br>' +
+    '- Trọng lượng: 152g'+'<br>' +
+    'DAO SANTOKU THÁI ĐA NĂNG:' +'<br>' +
+    '- Chiều dài lưỡi: 165 mm' +'<br>' +
+    '- Chiều rộng lưỡi: 47 mm' +'<br>' +
+    '- Tổng chiều dài: 295 mm' +'<br>' +
+    '- Độ dày sống dao: 1-1.5 mm' +'<br>' +
+    '- Độ dày cán dao: 10 mm' +'<br>' +
+    '- Trọng lượng: 157g' + '<br>' +
+    'DAO PETTY GỌT HOA QUẢ:' + '<br>' +
+    '- Chiều dài lưỡi: 120mm' + '<br>' +
+    '- Chiều rộng lưỡi: 26mm' + '<br>' +
+    '- Tổng chiều dài: 240mm' + '<br>' +
+    '- Độ dày lưỡi: 1mm' + '<br>' +
+    '- Độ dày cán dao: 10mm' + '<br>' +
+    '- Trọng lượng: 73g'
+}
+
+let sp4 = {
+    masp: 4,
+    tensp: "BỘ 3 CHIẾC DAO BẾP CAO CẤP THƯƠNG HIỆU KATANA ESSENTIAL - DAO CHẶT - DAO THÁI - DAO GỌT HOA QUẢ - KATASET001",
+    hinhanh: './img/Dao/daochat3.webp',
+    soluong: 5,
+    dongia: 159000,
+    mota:
+        'Thông tin sản phẩm:' +'<br>' +
+        'DAO CHẶT CLEVER:' +'<br>' +
+        '- Chiều dài lưỡi: 175mm' +'<br>' +
+        '- Chiều rộng lưỡi: 90mm' +'<br>' +
+        '- Tổng chiều dài: 310mm' +'<br>' +
+        '- Độ dày lưỡi: 4mm'+'<br>' +
+        '- Độ dày cán dao: 17mm' + '<br>' +
+        '- Độ cứng: 53HRC' + '<br>' +
+        '- Chất liệu: SUS420-J2' + '<br>' +
+        '- Trọng lượng: 596g' + '<br>' +
+        'DAO SANTOKU THÁI ĐA NĂNG:' +'<br>' +
+        '- Chiều dài lưỡi: 165 mm' +'<br>' +
+        '- Chiều rộng lưỡi: 47 mm' +'<br>' +
+        '- Tổng chiều dài: 295 mm' +'<br>' +
+        '- Độ dày sống dao: 1-1.5 mm' +'<br>' +
+        '- Độ dày cán dao: 10 mm' +'<br>' +
+        '- Trọng lượng: 157g' + '<br>' +
+        'DAO PETTY GỌT HOA QUẢ:' + '<br>' +
+        '- Chiều dài lưỡi: 120mm' + '<br>' +
+        '- Chiều rộng lưỡi: 26mm' + '<br>' +
+        '- Tổng chiều dài: 240mm' + '<br>' +
+        '- Độ dày lưỡi: 1mm' + '<br>' +
+        '- Độ dày cán dao: 10mm' + '<br>' +
+        '- Trọng lượng: 73g'
+}
+
+let sp9 = {
+    masp: 9,
+    tensp: 'Bộ nồi 4 món Fissler OPC2 Nắp inox',
+    hinhanh: './img/Noi/bo-noi-4-mon-fissler-opc2-nap-inox-1.jpg',
+    soluong: 5,
+    dongia: 19470000,
+    mota: 'Bộ nồi 4 món gồm: nồi cao 16cm - 20cm - 24 cm, quánh 16cm' +'<br>' +
+        '-Vật liệu từ thép không gỉ 18/10 cao cấp, an toàn cho sức khỏe' +'<br>' +
+        '-Công nghệ đáy chảo Cookstar giúp hấp thụ và truyền nhiệt tốt' +'<br>' +
+        '-Thiết kế nắp nồi giúp lưu giữ hương vị và chất dinh dưỡng món ăn' +'<br>' +
+        '-Thang đo dung tích bên trong nồi giúp dễ dàng định lượng khi nấu' +'<br>' +
+        '-Tay cầm cực kì chắc chắn, vành nồi rộng giúp dễ dàng rót thức ăn'
+}
+
+let sp10 = {
+    masp: 10,
+    tensp: 'BỘ NỒI 4 MÓN FISSLER HAMBURG',
+    hinhanh: './img/Noi/bo-4-noi-thep-fissler-hamburg.jpg',
+    soluong: 5,
+    dongia: 7990000,
+    mota: 'Bộ nồi 4 món với đa dạng kích thước, tiện dụng' +'<br>' +
+        'Chất liệu thép không gỉ 18/10 cao cấp, tỏa nhiệt đều' +'<br>' +
+        'Cấu tạo đáy nồi 3 lớp kim loại dày, hấp thụ nhiệt tốt' +'<br>' +
+        'Công nghệ đáy nồi Superthermic sử dụng trên mọi loại bếp' +'<br>' +
+        'Nắp kính cường lực kín chặt, chịu nhiệt cao, chống tràn'+'<br>' +
+        'Bên trong nồi có vạch đo thể tích, tiện lợi đo lường' +'<br>' +
+        'Tay cầm cách nhiệt, chịu lực, an toàn khi sử dụng' 
+}
+
+let sp11 = {
+    masp: 11,
+    tensp: 'Bộ nồi chảo chống dính Baby Elmich EL-1167 (16cm)',
+    hinhanh: './img/Noi/bo-noi-chao-chong-dinh-baby-elmich-el-1167-16cm.jpg',
+    soluong: 5,
+    dongia: 499000,
+    mota: 'Gồm 1 nồi và 1 chảo với đa dạng kích thước tiện lợi' +'<br>' +
+        'Chất liệu hợp kim nhôm nguyên chất độ bền cao, dễ vệ sinh' +'<br>' +
+        'Lớp chống dính Whitford 304 cao cấp, an toàn' +'<br>' +
+        'Khả năng truyền nhiệt và giữ nhiệt lâu' +'<br>' +
+        'Không sinh chất độc hại, an toàn cho sức khỏe'+'<br>' +
+        'Sử dụng được với nhiều loại bếp khác nhau'
+}
+
+let sp12 = {
+    masp: 12,
+    tensp: 'Chảo inox Fissler Original Pro size 28cm',
+    hinhanh: './img/Noi/chao-inox-fissler-original-pro-size-28cm-1.jpg',
+    soluong: 5,
+    dongia: 5830000,
+    mota: 'Chảo inox Fissler có thiết kế đẹp mắt kích thước 28cm' +'<br>' +
+        'Nằm trong Series chảo cao cấp nhất của hãng Fissler' +'<br>' +
+        'Chất liệu inox cao cấp 18/10 không gỉ, an toàn sức khỏe' +'<br>' +
+        'Công nghệ đáy chảo Cookstar giúp truyền dẫn nhiệt tốt' +'<br>' +
+        'Lòng chảo Novogrill phù hợp với những món chiên xào'+ '<br>' +
+        'Tay cầm chống bỏng, thành chảo có thang đo dung lượng'+'<br>' +
+        'Sản phẩm sử dụng được với mọi loại bếp, kể cả bếp từ'
+}
+
+let sp13 = {
+    masp: 13,
+    tensp: 'Lò vi sóng Electrolux 30 lít EMSB30XCF',
+    hinhanh: './img/Thietbidien/10054108-lo-vi-song-electrolux-emsb30xcf-1.jpg',
+    soluong: 5,
+    dongia: 10990000,
+    mota: 'Kiểu dáng hiện đại, thời thượng thích hợp với mọi không gian sống' +'<br>' +
+    'Dung tích lò đến 30 lít đáp ứng tốt nhu cầu nấu nướng của người dùng   ' +'<br>' +
+    'Công suất nướng 1250W mạnh mẽ và công suất nấu vi sóng 900W' +'<br>' +
+    '5 mức điều chỉnh nhiệt độ phù hợp chế biến nhiều món ăn khác nhau' +'<br>' +
+    'Chức năng chiên không dầu giúp hạn chế dầu mỡ, tốt cho sức khỏe'+ '<br>' +
+    'Nướng với quạt đối lưu cho món quay, nướng chín đều nhanh chóng'+'<br>' +
+    'CombiGrill kết hợp nướng và vi sóng trong một chương trình tiện lợi'
+}
+let sp14 = {
+    masp: 14,
+    tensp: 'Nồi cơm điện Zojirushi 1.8 lít NP-HRQ18-XT',
+    hinhanh: './img/Thietbidien/10025825-noi-com-dien-zojirushi-1-8-lit-np-hrq18-xt-1.jpg',
+    soluong: 5,
+    dongia: 18990000,
+    mota: 'Thiết kế sang trọng, phù hợp với mọi không gian bếp' +'<br>' +
+    'Dung tích 1.8 lít nấu được cơm cho 3-5 người ăn' +'<br>' +
+    'Lòng nồi mạ bạch kim làm tăng vị ngon của gạo' +'<br>' +
+    'Tích hợp nhiều chế độ nấu và chức năng hẹn giờ tiện lợi' +'<br>' +
+    'Sử dụng công nghệ cảm ứng nhiệt hiện đại'+ '<br>' +
+    'Màn hình LCD hiển thị thao tác, dễ dàng theo dõi, điều chỉnh'+'<br>' +
+    'Có thể dùng để nấu gạo lứt'
+}
+
+let sp15 = {
+    masp: 15,
+    tensp: 'Máy hút mùi Smeg KBT900XE',
+    hinhanh: './img/Thietbidien/may-hut-mui-smeg-kbt900xe-1.jpg',
+    soluong: 5,
+    dongia: 16990000,
+    mota: 'Thiết kế tinh tế, sang trọng thích hợp với mọi không gian' +'<br>' +
+    'Công suất hút xả mạnh mẽ, đạt tối đa lên đến 730 m3/h' +'<br>' +
+    'Điều khiển nút nhấn với 3 tốc độ hút mạnh/ nhẹ tùy chỉnh' +'<br>' +
+    '1 tốc độ hút tăng cường với khả năng hút mùi/khói mạnh mẽ' +'<br>' +
+    'Trang bị 2 đèn LED 2W chiếu sáng khu vực nấu ăn tiện lợi'
+}
+
+let sp16 = {
+    masp: 16,
+    tensp: 'Bình đun Smeg 1.7 lít KLF03RDEU',
+    hinhanh: './img/Thietbidien/10053134-binh-dun-smeg-lit-klf03rdeu-2.jpg',
+    soluong: 5,
+    dongia: 4890000,
+    mota: 'Thiết kế thời trang với thân bình màu đỏ và miệng rót màu bạc sang trọng' +'<br>' +
+    'Công suất mạnh mẽ lên đến 2400 W đun sôi nước nhanh, tiết kiệm thời gian' +'<br>' +
+    'Đế ấm xoay 360° tiện lợi cùng chân chống trượt đảm bảo an toàn tuyệt đối' +'<br>' +
+    'Bộ lọc vôi bằng thép không gỉ có thể tháo rời giúp lọc cặn đá vôi trong nước' +'<br>' +
+    'Bình được trang bị tính năng tự động tắt an toàn, tự động tắt khi đạt 100°C'
+}
+
+let sp17 = {
+    masp: 17,
+    tensp: 'Nồi chiên không dầu Philips 7.3 lít HD9860/90',
+    hinhanh: './img/Thietbidien/10048953-noi-chien-khong-dau-philips-7-3-lit-hd9860-90-1.jpg',
+    soluong: 5,
+    dongia: 6990000,
+    mota: 'Dung tích nồi chiên 7.3L, lòng nồi 3.5L, trọng lượng chứa thực phẩm 1.4kg' +'<br>' +
+    'Công nghệ cảm biến thông minh chế biến món chiên thơm ngon, giòn rụm' +'<br>' +
+    'Nồi chiên không dầu có công suất mạnh mẽ 2225W giúp thức ăn nhanh chín' +'<br>' +
+    'Dễ dàng điều chỉnh nhiệt độ 40-200 độ C, thời gian nấu (tối đa) 60 phút' +'<br>' +
+    'Nồi chiên giữ thức ăn luôn ấm và ở nhiệt độ lý tưởng trong tối đa 30 phút'+ '<br>' +
+    'Lồng nồi thiết kế hình sao giúp thực phẩm chín đều, giảm 90% lượng dầu mỡ'
+}   
+
+
+let sp18 = {
+    masp: 18,
+    tensp: 'Máy rửa chén Electrolux 1950W ESF5512LOX',
+    hinhanh: './img/Thietbidien/10042404-may-rua-chen-electrolux-1950w-esf5512lox-2_klin-2s.jpg',
+    soluong: 5,
+    dongia: 20490000,
+    mota: 'Độ ồn thấp 47 dB vận hành cực kì êm ái, bền bỉ' +'<br>' +
+    '6 chương trình rửa cài đặt khác nhau phù hợp nhu cầu sử dụng   ' +'<br>' +
+    'Chức năng AirDry tự động mở cửa khi chén bắt đươc rửa xong' +'<br>' +
+    'Hiệu suất rửa cao chứa đến 13 bộ tiết kiệm điện và nước tối đa' +'<br>' +
+    'Tự ngắt nước an toàn, chống tràn và rò rỉ thông minh'
+}
+
+let sp19 = {
+    masp: 19,
+    tensp: 'LẨU ĐIỆN TIGER QUEEN SQ-C350',
+    hinhanh: './img/Thietbidien/laudien.jpg',
+    soluong: 5,
+    dongia: 2390000,
+    mota: 'Dung tích: 4L và 1.5L' +'<br>' +
+    'Công suất: 1300W' +'<br>' +
+    'Chất liệu nồi: Hợp kim nhôm chống dính' +'<br>' +
+    'Nắp kính trong suốt'
+}
+
+let sp20 = {
+    masp: 20,
+    tensp: 'THỚT GỖ TEAK CAO CẤP KATANA CỠ LỚN KT05 - CHỮ NHẬT KÍCH THƯỚC 38X280X400MM',
+    hinhanh: './img/Vatdungkhac/thotgo.webp',
+    soluong: 5,
+    dongia: 600000,
+    mota: 'Kích thước: 38x280x400 mm' +'<br>' +
+    'Trọng lượng:  2795g' +'<br>' +
+    'Chất liệu: gỗ Teak nhập khẩu từ Lào 100%' +'<br>' +
+    'Hình thức sang trọng nhờ các kiểu dáng, màu gỗ, vân gỗ càng sáng bóng khi được phơi sáng'+'<br>' +
+    'Thớt gỗ Teak đạt tiêu chuẩn ISO 16649 - 2:2001, ISO 4832:2007'
+}
+
+let sp21 = {
+    masp: 21,
+    tensp: 'ĐÁ MÀI DAO TINH NANIWA ĐỘ MỊN #12000 (210X70X20)',
+    hinhanh: './img/Vatdungkhac/da-mai-tinh-naniwa-do-min-12000-210x70x20-s.webp',
+    soluong: 5,
+    dongia: 2159200,
+    mota: 'Kích thước: 210x70x20 mm' +'<br>' +
+    'Trọng lượng: 620g' +'<br>' +
+    'Loại đá mài: Đá mài nước' +'<br>' +
+    'Màu sắc: Màu vàng nhạt' +'<br>' +
+    'Độ mịn: #12000'+ '<br>' +
+    'Chất liệu cao cấp, đảm bảo an toàn và vệ sinh trong quá trình sử dụng.'+ '<br>' +
+    'Đá có độ mịn cao, nhanh chóng mài đi vết xước giúp dao trở nên sắc bén, sáng bóng.'+ '<br>' +
+    'Màu sắc trang nhã, tinh tế.'
+}
+
+let sp22 = {
+    masp: 22,
+    tensp: 'KÉO BẾP NHẬT CAO CẤP GLOBAL - KÉO CẮT GÀ GKS210',
+    hinhanh: './img/Vatdungkhac/gks210.webp',
+    soluong: 5,
+    dongia: 2249100,
+    mota: 'Kích thước: 200mm' +'<br>' +
+    'Trọng lượng: 299g' +'<br>' +
+    'Thiết kế nguyên khối sang trọng, chống giắt bụi bẩn' +'<br>' +
+    'Tay cầm bằng thép không gỉ đúc, hình tròn lõm vào chống trơn trượt'
+}
+let sp23 = {
+    masp: 23,
+    tensp: 'ĐŨA ĂN BẠC TA, GỖ ĐÀN HƯƠNG ĐỎ, GỖ MUN CAO CẤP SANG TRỌNG LILI_237599',
+    hinhanh: './img/Vatdungkhac/Dua-an-bac-ta-go-dan-huong-go-mun-cao-cap-sang-trong-LILI_237599_4.jpg',
+    soluong: 5,
+    dongia: 6174000,
+    mota: 'Chiều dài: 25 cm' +'<br>' +
+    'Loại: a bạc' +'<br>' +
+    'Màu sắc: Bạc, đen, đỏ gỗ' +'<br>' +
+    'Chất liệu: Bạc ta 999, gỗ đàn hương đỏ, gỗ mun'+'<br>' +
+    'Độ hoàn thiện: Xuất sắc'
+}
+let dssp = [sp1, sp2, sp3, sp4, sp9, sp10, sp11, sp12, sp13, sp14, sp15, sp16, sp17, sp18, sp19, sp20, sp21, sp22, sp23]
+
+
+localStorage.setItem("dssp", JSON.stringify(dssp))
